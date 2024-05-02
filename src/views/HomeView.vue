fb55ecdef352ad48067a7c9b8b5762d8d36cc479
usernameB99 <kum-a-saufn@live.at>
2024-05-02 19:55:09 +0200

    just a commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <RecipeItem 
+  :image="recipe.recipe.image" 
+  :label="recipe.recipe.label" 
+  :attributes="recipe.recipe.dietLabels.join(', ')" 
+  :url="recipe.recipe.url"
+  v-for="recipe in recipes" />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import RecipeItem from '@/components/RecipeItem.vue';
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
+    RecipeItem
+  },
+  data() {
+    return {
+      recipes: []
+    }
+  },
+  methods: {
+    async getRecipes() {
+      const query = 'sushi';
+      const appId = 'e079e210';
+      const appKey = '496d2aa4bf48ba186176e315e9b185bb';
+      const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
+      let resp = await fetch(url);
+      this.recipes = (await resp.json()).hits;
+    }
+  },
+  mounted() {
+    this.getRecipes();
   }
 }
 </script>
